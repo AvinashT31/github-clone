d88fc069d22101e11794f3306422ebf66b3b983a
AvinashT31 <59430438+AvinashT31@users.noreply.github.com>
2023-06-25 17:44:59 +0530

    update

--- github/src/Component/Home.jsx
+++ github/src/Component/Home.jsx
@@ -2,9 +2,43 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div  className='bg-slate-950 border-8 border-white w-full h-screen'>
-            <div>
-                <h1 className='text-white'>heloo</h1>
+        <div className='border border-black  w-full h-screen'>
+            <div className='border border-black w-full h-16'>
+                <div className='w-11/12 m-auto mt-2 h-11 flex justify-between'>
+                    <div className='w-2/4 h-full flex'>
+                        <div className='w-10 h-full'>
+                            <svg height="32" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="32" data-view-component="true" class="octicon octicon-mark-github">
+                                <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
+                            </svg>
+                        </div>
+                        <div className='w-24 h-full flex justify-center'>
+                            <p className='mt-1'>Product</p>
+                        </div>
+                        <div className='w-24 h-full flex justify-center'>
+                            <p className='mt-1'>Solutions</p>
+                        </div>
+                        <div className='w-32 h-full flex justify-center'>
+                            <p className='mt-1'>open source</p>
+                        </div>
+                        <div className='w-24 h-full flex justify-center'>
+                            <p className='mt-1'>Pricing</p>
+                        </div>
+                    </div>
+                    <div className='w-2/5 h-full flex'>
+                        <div className='border border-black w-60 h-8 mt-1 flex rounded-xl'>
+                            <input className='w-40 border-none outline-none ml-2' type="text" placeholder='Search Github' />
+                        </div>
+                        <div className='w-16 h-full ml-2'>
+                            <p className='mt-1'>Sign in</p>
+                        </div>
+                        <div className='border border-black w-16 h-8 mt-1 rounded-xl ml-2'>
+                            <p>Sign up</p>
+                        </div>
+
+                    </div>
+
+                </div>
+
             </div>
         </div>
     )
