651aa5e4ec312bbd8cb9972b42bc371a8cdcefea
AvinashT31 <59430438+AvinashT31@users.noreply.github.com>
2023-06-30 17:21:47 +0530

    update

--- github/tailwind.config.js
+++ github/tailwind.config.js
@@ -3,13 +3,22 @@ module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      height : {
-        '128' : '45rem',
-        '138' : '52rem',
+      height: {
+        '128': '48rem',
+        '138': '40rem',
+        '139': '41.5rem',
+        '140': '28rem', 
+      },
+      backgroundSize: {
+        '100': '100%',
+      },
+      width: {
+        '128': '95%',
+        '129': '0.4rem',
       }
-      
     },
   },
   plugins: [],
-}
+};
+
 
